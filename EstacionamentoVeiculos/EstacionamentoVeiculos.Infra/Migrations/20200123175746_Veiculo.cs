4864d99202f266d4995c009886ace6568bfe3eac
Gabriel de Oliveira <gabriel-ao@hotmail.com>
2020-01-23 15:00:13 -0300

    organziando migration Veiculo

---
+++ EstacionamentoVeiculos/EstacionamentoVeiculos.Infra/Migrations/20200123175746_Veiculo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EstacionamentoVeiculos.Infra.Migrations
+{
+    public partial class Veiculo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Veiculo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(nullable: false),
+                    UpdateDate = table.Column<DateTimeOffset>(nullable: true),
+                    Active = table.Column<bool>(nullable: false),
+                    Modelo = table.Column<string>(nullable: true),
+                    Marca = table.Column<string>(nullable: true),
+                    Placa = table.Column<string>(nullable: true),
+                    IdUsuario = table.Column<Guid>(nullable: false),
+                    UsuarioId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Veiculo_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculo_UsuarioId",
+                table: "Veiculo",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veiculo");
+        }
+    }
+}
